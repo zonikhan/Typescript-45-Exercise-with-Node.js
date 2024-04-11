d85aadf1857e43344f34c274f492f452395f1083
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-12 01:16:55 +0500

    45 Assignment

---
+++ Q0036/main.js
@@ -0,0 +1,5 @@
+function make_shirt(size, printMessage) {
+    console.log("you Selected ".concat(size, " size shirt with ").concat(printMessage, " print on shirt"));
+}
+make_shirt("Medium", "Code with Hamza");
+make_shirt("small", "Rabia");
---
+++ Q0036/main.ts
@@ -0,0 +1,8 @@
+
+function make_shirt ( size:string,printMessage: string){
+    console.log(`you Selected ${size} size shirt with ${printMessage} print on shirt`)
+
+}
+make_shirt("Medium", "Code with Hamza")
+
+make_shirt("small", "Rabia")
