e5aedbb4bbb1b36b270ad45a42f7c4750e8da7ae
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-11 23:45:38 +0500

    45 Assignment

---
+++ Q0022/main.js
@@ -0,0 +1,6 @@
+// Create a Array
+var errorArray = ["A", "B", "C", "D"];
+// producing Error ! by Accessing invalid index of array
+// console.log(errorArray[10]);
+//
+console.log(errorArray[1]);
---
+++ Q0022/main.ts
@@ -0,0 +1,9 @@
+// Create a Array
+let errorArray: string[] = ["A","B","C","D"];
+
+// producing Error ! by Accessing invalid index of array
+// console.log(errorArray[10]);
+
+// Error Removed
+
+console.log(errorArray[1]);
