ab3bb62b000919de5bc6e6fc81a6317125965f5e
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-12 00:18:53 +0500

    45 Assignment

---
+++ Q0028/main.ts
@@ -0,0 +1,25 @@
+
+//Creating a varibal
+let age = 100;
+
+// Creating a program for Determing the stage of life using if -else Chain
+
+if (age < 2){
+    console.log("you are a Baby,");
+}
+else if (age >= 2 && age < 4){
+    console.log("you are a Toddler.");
+}
+else if(age >= 4 && age < 13){
+    console.log("you are a Kid,");
+}
+else if (age >= 13 && age < 20){
+    console.log("you are Teenager,");
+}
+else if (age >= 20 && age < 65){
+    console.log("you are a Adultes");
+}
+
+else if (age >= 65){
+    console.log("you are Elders,");
+}
