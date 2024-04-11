acaaa518cdc0e73edb61f2f0b75d90a8a5d59aed
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-11 23:10:44 +0500

    45 Assignment

---
+++ Q0014/main.js
@@ -0,0 +1,5 @@
+//Guest List:if you could invite anyone,living or deceased,to dinner,who would you invite?
+//Make a list that includes at least three people you 'd like to inivite to diner. then use your list to print a message to each person,
+// inviting them to dinner.
+var guestList = ["hamza", "Usman", "Ayesha", "Hani"];
+guestList.forEach(function (oneGuest) { return console.log("Salam ".concat(oneGuest, " ,would you like to dinner withme?")); });
---
+++ Q0014/main.ts
@@ -0,0 +1,9 @@
+
+//Guest List:if you could invite anyone,living or deceased,to dinner,who would you invite?
+//Make a list that includes at least three people you 'd like to inivite to diner. then use your list to print a message to each person,
+// inviting them to dinner.
+
+
+let guestList = [ "hamza","Usman","Ayesha","Hani"];
+
+guestList.forEach(oneGuest => console.log(`Salam ${oneGuest} ,would you like to dinner withme?`));
