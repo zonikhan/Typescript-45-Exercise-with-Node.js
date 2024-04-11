bda821d5118bc4eee9e9745ba89cfbdbca365a2f
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-12 00:16:19 +0500

    45 Assignment

---
+++ Q0027/main.ts
@@ -0,0 +1,37 @@
+// Define varible
+let alienColor = "green";
+// using if and Else - if statement
+if (alienColor ==="green"){
+console.log("(varsion 1)you shot dwon green slien you have earned 5 point");
+}
+else if(alienColor === "yellow"){
+    console.log("(varsion 2 )you shot down yellow alien you have earned 10 points");
+}
+else if(alienColor === "red"){
+    console.log("you shot dwon red alien you have earned 15 points");
+
+}
+// VERSION 2
+let alienColor2 = "yellow"
+
+if (alienColor2 ==="green"){
+    console.log("you shot down green alien you have earned 5 points");
+}
+else if(alienColor2 ==="yellow"){
+    console.log("you show down yellow alien you earned 10 point");
+}
+else if(alienColor2 === "red"){
+    console.log("you shot down red alien you have earned 15 point");
+}
+// version
+let alienColor3 = "red";
+if(alienColor3 === "green"){
+    console.log("you shot down green you have earned 5 points");
+}
+else if(alienColor3 === "yellow"){
+
+console.log("you shot down yellow alien you have earned 10 points");
+}
+else if(alienColor3=== "red"){
+    console.log("(version3)you shot down red alien you have earned 15 points");
+}
