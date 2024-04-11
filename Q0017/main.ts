b79e44635a7fca042d4c81a9e38b06a1ca506d66
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-11 23:22:31 +0500

    45 Assignment

---
+++ Q0017/main.ts
@@ -0,0 +1,52 @@
+// creating a Guest List Arry 
+ 
+let guestlist =[ "sonu","ahsan","Usman","Hamza"];
+
+//Making varibal for those guest who cant come
+
+ let donCome = guestlist[0];
+//print the name of guest who cant come
+
+ console.log(donCome,"Nai Ahh sakte hai");
+// Add or Remove values from Guest List Arry
+ guestlist.splice(0,1,"Amir");
+// Message prient for Bigger Table
+
+ console.log("Good New !we have Found a Bigger Table For Dinner.") ;
+  // Adding a new guest at starting index of array
+ guestlist.unshift("Ali");
+
+ // Adding a new guest at ending index of array
+  
+ guestlist.push("zain");
+ 
+// Making a varable for  storing  a middle index of our guest list array
+
+ let middleIndex: number = Math.floor(guestlist.length / 2);
+
+ // Adding a new guest to middil index of array
+ guestlist.splice(middleIndex,0,"Osama");
+
+ // print Message of update  List
+ console.log("Update List of our Guests");
+ // seding a invitation message to our guests one by one  with  their names
+
+ guestlist.forEach(oneguest => console.log(`salam ${oneguest}, would you like to dinner with me`));
+
+ // Inform that only two guest  can be invited for dinner
+
+ console.log("Unfortunately,the new dinner table wont arrive on time,so I can only invite twoGuests to dinner with me");
+
+ // using while-loop to remove guests from the array until only two name remain
+ while(guestlist.length > 2) {
+    let removedGuest = guestlist.pop();
+    console.log(`sorry,the ${removedGuest}I can inite you tod dinner`);
+ }
+// sending q inivitation to the last two guest on the list
+ console.log("Invitations to the last 2 Guest");
+guestlist.forEach(lasttwo => console.log(`Lucky ${lasttwo}, you are still inviteted to dinner`));
+// removing last two guest from the list
+guestlist.pop();
+guestlist.pop();
+
+console.log("Empty List:", guestlist);
