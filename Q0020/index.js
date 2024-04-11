330aa64ef64407f1298fe78c900ef921dd33e2ba
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-11 23:34:48 +0500

    45 Assignment

---
+++ Q0020/index.js
@@ -0,0 +1,7 @@
+// Making a programming Languages Array
+var programmingLanguages = ["typescript", "javaScript", "python", "PHP"];
+// print the Message of List
+console.log("List of Programming Languages");
+// print the values of Array in the form of List
+programmingLanguages.forEach(function (language) { return console.log(language); });
+// console.log(programmingLanguages);
