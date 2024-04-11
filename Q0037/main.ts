a239823887e4012fa302bb41ffdd59ed91aa7436
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-12 01:19:25 +0500

    45 Assignment

---
+++ Q0037/main.ts
@@ -0,0 +1,14 @@
+
+// Making a function
+function make_shirt (size: string ="Large", printMessage: string = "I Love TypesSript"){
+console.log(`Creating a ${size} shirt with the ${printMessage} print on shirt`)
+}
+
+//Calling a funtion with by - defult valus
+make_shirt();
+
+// Calling a funtion now with Medium size and default- message
+make_shirt("Medium")
+
+// Calling a function now small size and Different print Message
+make_shirt("small","I Love javaScript")
