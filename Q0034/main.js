b9771e08a08ef6ddda91b785d7160ff54cc94d33
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-12 01:08:53 +0500

    45 Assignment

---
+++ Q0034/main.js
@@ -0,0 +1,10 @@
+// Creating  a arry
+var pizza = ["chicken Tika", "Malai", "cheese", "Fajita"];
+//using for loop
+for (var _i = 0, pizza_1 = pizza; _i < pizza_1.length; _i++) {
+    var onepizza = pizza_1[_i];
+    console.log("I like ".concat(onepizza, " pizza"));
+}
+//  print a message outside of the - loop
+console.log("pizza is Love");
+console.log("I eat pizza");
---
+++ Q0034/main.ts
@@ -0,0 +1,14 @@
+// Creating  a arry
+let pizza = ["chicken Tika","Malai","cheese","Fajita"]
+
+//using for loop
+
+for(let onepizza of pizza){
+console.log(`I like ${onepizza} pizza`);
+
+}
+ //  print a message outside of the - loop
+ console.log("pizza is Love") 
+ console.log("I eat pizza")
+
+
