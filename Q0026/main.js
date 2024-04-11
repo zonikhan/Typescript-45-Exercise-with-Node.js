54eb6ce2f0060e88ba646759e0ee9604ac3a4338
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-12 00:12:42 +0500

    45 Assignment

---
+++ Q0026/main.js
@@ -0,0 +1,15 @@
+// Define varible
+var alienColor = "green";
+if (alienColor === "green") {
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
+//  second varison
+if (alienColor === "blue") {
+    console.log("I am come from Else Statment");
+}
+else {
+    console.log("I am come from Else Statment.");
+}
---
+++ Q0026/main.ts
@@ -0,0 +1,19 @@
+// Define varible
+let alienColor = "green";
+// first varsion
+if (alienColor === "green"){
+    console.log("player just earned 5 points for shooting the alien.")
+
+}
+else{
+    console.log("player just earned 10 points.");
+}
+
+//  second varison
+if (alienColor === "blue"){
+    console.log("I am come from Else Statment");
+    
+}
+else{
+     console.log("I am come from Else Statment.");
+}
