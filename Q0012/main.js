3bcfcb5f6365e3b0df7f300cfc386488ff2c76da
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-11 23:05:30 +0500

    45 Assignment

---
+++ Q0012/main.js
@@ -0,0 +1,2 @@
+var friendName = ["Aisha", "Usaman", "Saniya", "Rani"];
+friendName.forEach(function (friendName) { return console.log("Hellow ".concat(friendName, ",How are you?")); });
---
+++ Q0012/main.ts
@@ -0,0 +1,4 @@
+
+let friendName = ["Aisha","Usaman","Saniya","Rani"];
+
+friendName.forEach(friendName => console.log(`Hellow ${friendName},How are you?`));
