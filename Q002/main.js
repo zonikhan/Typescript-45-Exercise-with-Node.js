3c53db3004cbd44c61d10139a2da2409f856f94a
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-11 21:14:29 +0500

    45 assignment

---
+++ Q002/main.js
@@ -0,0 +1,4 @@
+// personal mesage store aperson 's name is a varibal,and print a message to thaat person your message should be simple such as,hello
+// Eric would you like to laarn some python today?;
+var personName = "Rabia";
+console.log("hello", personName, "world you like to learn some python today?");
---
+++ Q002/main.ts
@@ -0,0 +1,6 @@
+// personal mesage store aperson 's name is a varibal,and print a message to thaat person your message should be simple such as,hello
+// Eric would you like to laarn some python today?;
+
+
+let personName = "Rabia";
+console.log("hello", personName,"world you like to learn some python today?");
