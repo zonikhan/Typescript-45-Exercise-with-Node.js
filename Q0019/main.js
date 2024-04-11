091574057ce6e8f02203037062a7f1ed8acaaf1e
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-11 23:31:36 +0500

    45 Assignment

---
+++ Q0019/main.js
@@ -0,0 +1,4 @@
+var guestList = ["Hamza", "Usman", "Ayesha", "Hani", "Sonia"];
+//  guestList.forEach(oneGuest => console.log(`Salam ${oneGuest} ,would you like to dinner withme?`));
+var lengthGuests = guestList.length;
+console.log("We are Inviting totol ".concat(lengthGuests, "guests."));
---
+++ Q0019/main.ts
@@ -0,0 +1,11 @@
+
+
+
+
+let guestList =   ["Hamza","Usman","Ayesha","Hani","Sonia"];
+
+//  guestList.forEach(oneGuest => console.log(`Salam ${oneGuest} ,would you like to dinner withme?`));
+
+let lengthGuests: number = guestList.length;
+
+console.log(`We are Inviting totol ${lengthGuests} guests.`);
