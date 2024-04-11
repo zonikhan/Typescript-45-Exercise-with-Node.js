e229feacf2bc66c7c7e999ec8ad1cdd453a2a071
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-11 23:37:24 +0500

    45 Assignment

---
+++ Q0021/main.js
@@ -0,0 +1,6 @@
+var itCourse = {
+    courseName: "typescrip and javaScript",
+    location: "Governer House Sindh",
+    onSiteStudents: 50000
+};
+console.log(itCourse);
---
+++ Q0021/main.ts
@@ -0,0 +1,11 @@
+interface itCourse {
+    courseName: string;
+    location: string;
+    onSiteStudents: number;
+}
+let itCourse ={
+    courseName: "typescrip and javaScript",
+    location: "Governer House Sindh",
+    onSiteStudents: 50000
+};
+console.log(itCourse);
