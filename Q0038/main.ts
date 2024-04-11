e58ba05a262dc2984309b1f7548085059ca41e18
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-12 01:22:39 +0500

    45 Assignment

---
+++ Q0038/main.ts
@@ -0,0 +1,10 @@
+// Describe a Function
+function describe_city (city: string, country: string = "pakistan"){
+    console.log(`${city} is in ${country}`);
+}
+// Calling the function
+describe_city("Karachi");
+
+describe_city("Lahore");
+
+describe_city("Berlin","Germany")
