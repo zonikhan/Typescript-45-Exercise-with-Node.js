5a0338e47fccafefac777e5b3a94da036538711c
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-11 22:00:32 +0500

    Add files via upload

---
+++ Q005/main.js
@@ -0,0 +1,5 @@
+// Fomouse Quote 2:Repeate Exercise 4, but this time store the famouse person's name in a varibal called famous_person.then compose
+// your message and store it in a new varibale called message.print your message.
+var famsous_person = "Adolf Hitler";
+var message = "if you want to shine like a sun you have to burn like it";
+console.log("".concat(famsous_person, " once said,\"").concat(message));
---
+++ Q005/main.ts
@@ -0,0 +1,13 @@
+// Fomouse Quote 2:Repeate Exercise 4, but this time store the famouse person's name in a varibal called famous_person.then compose
+// your message and store it in a new varibale called message.print your message.
+
+let  famsous_person: string = "Adolf Hitler";
+let message: string =
+"if you want to shine like a sun you have to burn like it";
+console.log(`${famsous_person} once said,"${message}`);
+
+
+
+
+
+
