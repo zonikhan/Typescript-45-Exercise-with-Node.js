655aff611d9703b3a8b2503ed91b7a33ff3ca558
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-12 00:47:18 +0500

    45 Assignment

---
+++ Q0029/main.js
@@ -0,0 +1,18 @@
+"use strict";
+// Creatina a Array
+let favorite_fruites = ["Mango", "Strawberry", "Orange"];
+if (favorite_fruites.includes("Mango")) {
+    console.log("I Really Like Mangos");
+}
+if (favorite_fruites.includes("Strawberry")) {
+    console.log("I Really like Strawberry");
+}
+if (favorite_fruites.includes("Apple")) {
+    console.log("I Really Like Apple");
+}
+if (favorite_fruites.includes("Orange")) {
+    console.log("I Really Like Orange");
+}
+if (favorite_fruites.includes("Banana")) {
+    console.log("I Really like Banana");
+}
