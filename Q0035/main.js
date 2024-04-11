095a6916fe0cf1511fe525778fb7e57c06f74f22
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-12 01:13:38 +0500

    45 Assignment

---
+++ Q0035/main.js
@@ -0,0 +1,9 @@
+// Creating A Array
+var petAnimals = ["Cat", "Dog", "Rabbit"];
+// using for - loop
+for (var _i = 0, petAnimals_1 = petAnimals; _i < petAnimals_1.length; _i++) {
+    var onepet = petAnimals_1[_i];
+    console.log("A ".concat(onepet, " would make a great pet."));
+}
+// print a message outside of - loop
+console.log("Any of these animals would make a great pet!\n");
