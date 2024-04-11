cdd8b03121646915a16e5eb6e16ed4ff5da7b7a8
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-12 01:35:16 +0500

    45 Assignment

---
+++ Q0041/main.js
@@ -0,0 +1,8 @@
+// Define a function to print rach magician name from a an array
+function show_magician(magicians) {
+    magicians.forEach(function (name) { return console.log(name); });
+}
+// Define an array containing magicians name
+var magician_names = ["Hurry poter", "Hamza", "Usman"];
+// Call the function to print each magician name
+show_magician(magician_names);
---
+++ Q0041/main.ts
@@ -0,0 +1,15 @@
+
+// Define a function to print rach magician name from a an array
+
+function show_magician(magicians: string[]){
+    magicians.forEach(name => console.log(name)); 
+        
+}
+// Define an array containing magicians name
+let magician_names = ["Hurry poter", "Hamza", "Usman"] 
+
+// Call the function to print each magician name
+
+show_magician(magician_names);
+
+
