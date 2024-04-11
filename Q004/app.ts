6055449439391c00effa53b75821de7404c01be7
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-11 22:24:50 +0500

    Add files via upload

---
+++ Q004/app.ts
@@ -0,0 +1,6 @@
+// Famouse Quote: Find a quote from a famous person you admire.print the quote and the name of its author.your output.should look
+//somthing like the folling.including the quotation marks:
+
+
+
+console.log("Albert Einstein onesaid,\"A person who never made a mastike never tried anything new.\"");
